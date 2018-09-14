a10fdd7194750adfdd0a34677fd9a8f03ab24d75
BraysonWheeler <35555839+BraysonWheeler@users.noreply.github.com>
2018-09-14 18:02:16 -0500

    Add files via upload

---
+++ Assignments/P01/main.cpp
@@ -0,0 +1,38 @@
+/**
+ *  Course: CMPS 2143 - OOP
+ *  Assignment: A01
+ *  Purpose: C++ class implements functionality of a c++ vector using an array
+ *           as a storage container. the vector will grow and shrink when neccessary
+ *           elements will be pushed on and poped off when told to.
+ *
+ *  @author Logan Wheeler
+ *  @version 1.1 09/04/18
+ */
+
+
+
+
+#include <iostream>
+#include "myVector.h"
+
+using namespace std;
+
+int main() {
+    
+    myVector V(10);
+    V.pop_back();
+    V.push_back(4);
+    V.push_back(13);
+    V.push_back(73);
+    V.print();
+    // Creating new array to fill
+    myVector B(10);
+    for(int i = 0;i < 10000;i++){
+        B.push_back(i);
+    }
+    
+    cout << "Vector size is " << B.vSize << endl;
+    //   for(int i = 0;i < 5;i++){
+    //       B.pop_back();
+    //   }
+}
---
+++ Assignments/P01/myVector.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include "myVector.h"
+
+using namespace std;
+
+/**
+ *  Creates the array with the size given in main
+ *
+ *  param (int) an int to create size of array
+ *  return (void) Nothing.
+ */
+
+myVector::myVector(int size){
+    vSize = size;
+    origsize = size;
+    theVector = new int[vSize];
+    for(int i= 0;i<vSize;i++){
+        theVector[i] = i;
+    }
+}
+
+/**
+ *  Prints out what is currently in the array
+ *
+ *  param () nothing
+ *  return (void) Nothing.
+ */
+
+void myVector::print(){
+    for(int i=0;i<index;i++){
+        cout<< theVector[i]<<" ";
+    }
+    cout << endl;
+}
+
+/**
+ *  Shrinks the array when called, cannot shrink an array past
+ *   original size
+ *
+ *  param () nothing
+ *  return (void) Nothing.
+ */
+
+void myVector::shrink(){
+    if(vSize == origsize){
+        
+    }
+    else{
+        vSize = vSize/2;
+        int *newarray = new int[vSize/2];
+        for(int i = 0; i < vSize; i++){
+            newarray[i] = theVector[i];
+        }
+        index = vSize;
+        delete[] theVector;
+        theVector = newarray;
+
+    }
+}
+/**
+ *  Pops an element off the back of an array, will test to see
+ *   if the array is empty if so will return 0
+ *
+ *  param () nothing
+ *  return (int) return the element.
+ */
+
+int myVector::pop_back(){
+    if(index == 0){
+        cout << "Cannot remove from empty Vector" << endl;
+        return 0;
+    }
+    else if(percentfull() <= vSize*.4){
+        cout <<"shrinking";
+        shrink();
+        return theVector[index--];
+    }
+    else{
+        return theVector[index--];
+    }
+}
+
+/**
+ * Function will grow the array to a size of 1.5
+ *
+ *  param () nothing
+ *  return (int) return the element.
+ */
+
+void myVector::push_back(int item){
+    if (index >= vSize){
+        vSize = vSize*1.5;
+        int *newarray = new int[vSize];
+        for(int i = 0; i < vSize;i++){
+            newarray[i] = theVector[i];
+        }
+        max = vSize;
+        theVector = newarray;
+        delete[] theVector;
+        cout << "resizing to " << vSize << endl;
+    }
+    
+    theVector[index++] = item;
+}
+
+/**
+ *  tests how full the array is in order to shrink or not shrink the array
+ *
+ *  param () nothing
+ *  return (float) decimal/percentage of how full the array is.
+ */
+
+
+float myVector::percentfull(){
+    float pfull = (float)index/vSize;
+    return pfull;
+}
+
