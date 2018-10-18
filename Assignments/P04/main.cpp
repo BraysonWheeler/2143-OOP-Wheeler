d0621e8ff99ce6940f41c97ea8d3c0b729c8f156
BraysonWheeler <35555839+BraysonWheeler@users.noreply.github.com>
2018-10-18 17:00:47 -0500

    Update main.cpp

--- Assignments/P04/main.cpp
+++ Assignments/P04/main.cpp
@@ -1,3 +1,17 @@
+/**
+ *  Course: CMPS 2143 - OOP
+ *  Assignment: A01
+ *  Purpose: The universe of the Game of Life is an infinite two-dimensional orthogonal grid of square cells, each of which is in one of two possible states, live or dead. Every cell interacts with its eight neighbors, which are the cells that are directly horizontally, vertically, or diagonally adjacent. At each step in time, the following transitions occur:
+ 
+        Any live cell with fewer than two live neighbors dies, as if caused by under-population.
+        Any live cell with more than three live neighbors dies, as if by overcrowding.
+        Any live cell with two or three live neighbors lives on to the next generation.
+        Any dead cell with exactly three live neighbors becomes a live cell.
+ *  @author Logan Wheeler
+ *  @version 1.1 10/15/18
+ */
+
+
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <fstream>
@@ -44,42 +58,39 @@ private:
     RenderWindow &WindowRef;    // reference to sfml window so we can draw to it.
     string line;
 
-//    /**
-//     * Creates the game board, resets it (0's it out), then randomly populates it.
-//     */
-//    void init() {
-//
-//        World = new lifeCell *[Rows];
-//
-//        for (int i = 0; i < Rows; i++) {
-//            World[i] = new lifeCell[Cols];
-//        }
-//
-//        resetBoard();
-//    }
     
 public:
-//    /**
-//     * GameOfLife Constructor extends sfml WindowRef
-//     * @param:
-//     *    RenderWindow : a reference to our sfml window
-//     *
-//     */
+    /**
+     * GameOfLife Constructor extends sfml WindowRef
+     * param:
+     *    RenderWindow : a reference to our sfml window
+     *
+     */
     gameOfLife(RenderWindow &thatWindow,ifstream &infile) : WindowRef(thatWindow) {
         initBoard(infile);
     }
 
-//    /**
-//     * GameOfLife Constructor extends sfml WindowRef
-//     * @param:
-//     *    RenderWindow : a reference to our sfml window
-//     *    int rows : num of rows in board
-//     *    int cols : num of cols in board
-//     *
-//     */
+    /**
+     * GameOfLife Constructor extends sfml WindowRef
+     * param:
+     *    RenderWindow : a reference to our sfml window
+     *    int rows : num of rows in board
+     *    int cols : num of cols in board
+     *
+     */
+    
     gameOfLife(RenderWindow &thatWindow, int rows, int cols,ifstream &infile) : WindowRef(thatWindow) {
         initBoard(infile);
     }
+        /**
+         * Function: print
+         *    Prints out the array in text format rather than SFML
+         *
+         * param:
+         *    None
+         * returns:
+         *       True or false
+         */
     
     void print(){
         for(int i = 0; i < Rows; i++){
@@ -89,19 +100,19 @@ public:
             cout << endl;
         }
     }
-    //    /**
-    //     * Function: rulse
-    //     *     Runs the rules GOL:
-    //     *        less than two the cell dies
-    //     *        more than three the cell dies
-    //     *        two or three the cell lives
-    //     *
-    //     *
-    //     * @param:
-    //     *    None
-    //     * @returns:
-    //     *       True or false
-    //     */
+        /**
+         * Function: rulse
+         *     Runs the rules GOL:
+         *        less than two the cell dies
+         *        more than three the cell dies
+         *        two or three the cell lives
+         *
+         *
+         * param:
+         *    None
+         * returns:
+         *       True or false
+         */
     
     void rules(){
         for(int i = 0; i < Rows; i++){
@@ -109,124 +120,102 @@ public:
                 if(World[i][j].neighborCount < 2){
                     World[i][j].alive = 0;
                 }
-                 if(World[i][j].neighborCount > 3){
+
+                if(World[i][j].neighborCount > 3){
                     World[i][j].alive = 0;
                 }
-                 if(World[i][j].neighborCount == 2 ||World[i][j].neighborCount == 3){
+                
+                if(World[i][j].neighborCount == 3 && World[i][j].alive == 0){
                     World[i][j].alive = 1;
                 }
             }
         }
-        
-        
     }
-    //    /**
-    //     * Function: onWorld
-    //     *     Tests if World[i][j] is on the board
-    //     * @param:
-    //     *    None
-    //     * @returns:
-    //     *       True or false
-    //     */
+        
+
+        /**
+         * Function: onWorld
+         *     Tests if World[i][j] is on the board
+         * param:
+         *    None
+         * returns:
+         *       True or false
+         */
     
     bool onWorld(int i,int j){
         return (i >=0 && i < Rows && j >= 0 && j < Cols);
     }
     
-    //    /**
-    //     * Function: neighborCount
-    //     *     Cheks to see if neighbors are on the world and if the cell is alive
-    //     *     increases the neighborcount
-    //     * @param:
-    //     *    None
-    //     * @returns:
-    //     *       None
-    //     */
+        /**
+         * Function: countNeighbors
+         *     Cheks to see if neighbors are on the world and if the cell is alive
+         *     increases the neighborcount
+         * param:
+         *    None
+         * returns:
+         *       None
+         */
     
-    void neighborCount(){
+    void countNeighbors(){
         for(int i = 0; i < Rows; i++){
             for(int j = 0; j < Cols; j++){
-                    if(onWorld(i+1,j+1) && World[i+1][j+1].alive == 1){
+                if(onWorld(i-1,j-1)){
+                    if(World[i-1][j-1].alive == 1){
                         World[i][j].neighborCount++;
                     }
-                    if(onWorld(i+1,j) && World[i+1][j].alive == 1){
+                }
+                if(onWorld(i-1,j)){
+                    if(World[i-1][j].alive == 1){
                         World[i][j].neighborCount++;
                     }
-                    if(onWorld(i,j+1) && World[i][j+1].alive == 1){
+                }
+                if(onWorld(i-1,j+1)){
+                    if(World[i-1][j+1].alive == 1){
                         World[i][j].neighborCount++;
                     }
-                    if(onWorld(i-1,j-1) && World[i-1][j-1].alive == 1){
+                }
+                if(onWorld(i,j-1)){
+                    if(World[i][j-1].alive == 1){
                         World[i][j].neighborCount++;
                     }
-                    if(onWorld(i-1,j) && World[i-1][j].alive == 1){
+                }
+                if(onWorld(i,j+1)){
+                    if(World[i][j+1].alive == 1){
                         World[i][j].neighborCount++;
                     }
-                    if(onWorld(i,j-1) && World[i][j-1].alive == 1){
+                }
+                if(onWorld(i+1,j-1)){
+                    if(World[i+1][j-1].alive == 1){
                         World[i][j].neighborCount++;
                     }
-                    if(onWorld(i-1,j+1) && World[i-1][j+1].alive == 1){
+                }
+                if(onWorld(i+1,j)){
+                    if(World[i+1][j].alive == 1){
                         World[i][j].neighborCount++;
                     }
-                    if(onWorld(i+1,j-1) && World[i+1][j-1].alive == 1){
+                }
+                if(onWorld(i+1,j+1)){
+                    if(World[i+1][j+1].alive == 1){
                         World[i][j].neighborCount++;
                     }
+                }
             }
         }
     }
     
-    
-//    /**
-//     * Function: resetBoard
-//     *     Sets the size of each rectangle to CELLSIZE (necessary?)
-//     *       Also sets each cell to NOT alive
-//     * @param:
-//     *    None
-//     * @returns:
-//     *       None
-//     */
-    void resetBoard() {
-        for (int i = 0; i < Rows; i++) {
-            for (int j = 0; j < Cols; j++) {
-                World[i][j].rect.setSize(Vector2f(CELLSIZE, CELLSIZE));
-                World[i][j].alive = 0;
-                World[i][j].rect.setPosition((i + 1)*CELLSIZE, (j + 1)*CELLSIZE);
-            }
-        }
-    }
-    
-//    /**
-//     * Function: randomlyPopulate
-//     *     Sets max_life number of cells to alive
-//     * @param:
-//     *    int max_life
-//     * @returns:
-//     *       None
-//     */
-    void randomlyPopulate(int max_life = 30) {
-        int count = 0;
-        int r, c;
-        while (count < max_life) {
-            r = rand() % Rows;
-            c = rand() % Cols;
-            if (World[r][c].alive == 0) {
-                World[r][c].alive = 1;
-                World[r][c].rect.setFillColor(Color::Green);
-                count++;
-            }
-        }
-    }
-    
-//    /**
-//     * Function: initBoard
-//     *     Prints board to an sfml window
-//     * @param:
-//     *    None
-//     * @returns:
-//     *       None
-//     */
+    /**
+     * Function: initBoard
+     *     intitilizes board from input file creates an array full of cells
+     * param:
+     *    None
+     * returns:
+     *       None
+     */
     void initBoard(ifstream &infile){
-        World = new lifeCell*[Rows];
+        
         infile >> Rows >> Cols;
+        World = new lifeCell*[Rows];
+
         for(int i=0;i< Rows;i++){
             World[i] = new lifeCell[Cols];
         }
@@ -242,14 +231,32 @@ public:
             
         }
     }
-    //    /**
-    //     * Function: printBoard
-    //     *     Prints board to an sfml window
-    //     * @param:
-    //     *    None
-    //     * @returns:
-    //     *       None
-    //     */
+    
+    /**
+     * Function: resetNeightborCount
+     *     resets NeighborCount in life cell to 0 used for checking neighbor count
+     * param:
+     *    None
+     * returns:
+     *       None
+     */
+    
+    void resetNeighborCount() {
+        for (int i = 0; i < Rows; i++) {
+            for (int j = 0; j < Cols; j++) {
+                World[i][j].neighborCount = 0;
+            }
+        }
+    }
+    
+        /**
+         * Function: printBoard
+         *     Prints board to an sfml window
+         * parm
+         *    None
+         * returns:
+         *       None
+         */
     
     void printBoard() {
         int x = 0;
@@ -268,7 +275,26 @@ public:
             }
         }
     }
+    
+    
+    /**
+     * Function: outfileBoard
+     *     Prints array to output file
+     * parm
+     *    None
+     * returns:
+     *       None
+     */
+    void outfileBoard(ofstream &outfile){
+        for (int i = 0; i < Rows; i++) {
+            for (int j = 0; j < Cols; j++) {
+                outfile << World[i][j].alive;
+            }
+            outfile << endl;
+        }
+    }
 };
+
 
 
 /**
@@ -288,13 +314,13 @@ struct directionType {
     }
 };
 
-///**
-// * directionType direction:
-// * @params:
-// *    int rows - number of rows in 2D array
-// *    int cols - numberof cols in 2D array
-// * @returns: **RectangleShape (pointer)
-// */
+/**
+ * directionType direction:
+ * params:
+ *    int rows - number of rows in 2D array
+ *    int cols - numberof cols in 2D array
+ * returns: **RectangleShape (pointer)
+ */
 directionType direction(CircleShape shape, Vector2u winSize) {
     Vector2f position = shape.getPosition();
     
@@ -318,18 +344,25 @@ directionType direction(CircleShape shape, Vector2u winSize) {
 }
 
 
-int main() {
+int main(int argc, char** argv) {
+    // input output initilization
     ifstream infile;
-    infile.open("gen_zero.txt");
+    infile.open(argv[1]);
+    ofstream outfile;
+    outfile.open("output.txt");
     RenderWindow window(VideoMode(600, 600), "Game of Life");
     
     int frameRate = 1;
     int frameCount = 0;
     
+    // setting size of window
     Vector2u size = window.getSize();
     unsigned int width = size.x;
     unsigned int height = size.y;
-    gameOfLife Gol(window, width / CELLSIZE, height / CELLSIZE,infile);
+    gameOfLife Gol(window,infile);
+
+    // while window is open print board, count the neighbors, run through the rules,
+    // and resets the neighborCount to zero so we can loop again.
     
     while (window.isOpen()) {
         Event event;
@@ -338,18 +371,24 @@ int main() {
                 window.close();
         }
         window.clear();
-
         
-        Gol.neighborCount();
+        Gol.printBoard();
+        Gol.countNeighbors();
         Gol.rules();
-        Gol.print();
-
+        Gol.resetNeighborCount();
 
         if (frameCount % frameRate == 0) {
             window.display();
         }
 
         frameCount++;
+        
+        if(frameCount == 338){
+            break;
+        }
     }
+    
+    // outfiles board to output file
+    Gol.outfileBoard(outfile);
     return 0;
 }
